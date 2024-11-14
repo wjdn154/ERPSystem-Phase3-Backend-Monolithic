2eb3644a19f38a9e86838f0ce95633c4146ba18b
jh <chlwlgur0407@naver.com>
2024-11-14 10:34:16 +0900

    feature: AWS Xray SDK 설정

--- src/main/java/com/megazone/ERPSystem_phase3_Monolithic/common/config/SecretManagerConfig.java
+++ src/main/java/com/megazone/ERPSystem_phase3_Monolithic/common/config/SecretManagerConfig.java
@@ -1,61 +1,44 @@
-//package com.megazone.ERPSystem_phase3_Monolithic.common.config;
-//
-// import com.fasterxml.jackson.databind.JsonNode;
-// import com.fasterxml.jackson.databind.ObjectMapper;
-// import lombok.extern.slf4j.Slf4j;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Configuration;
-// import software.amazon.awssdk.regions.Region;
-// import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
-// import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
-// import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
-//
-//@Slf4j
-//@Configuration
-//public class SecretManagerConfig {
-//
-//    @Value("${AWS_REGION}")
-//    private String region;
-//
-//    public DatabaseCredentials getSecret() {
-//
-//        String secretName = "omz-env-secrets-backend";
-//        Region awsRegion = Region.of(region);
-//
-//        SecretsManagerClient client = SecretsManagerClient.builder()
-//                .region(awsRegion)
-//                .build();
-//
-//        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
-//                .secretId(secretName)
-//                .build();
-//
-//        GetSecretValueResponse getSecretValueResponse;
-//
-//        try {
-//            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
-//        } catch (Exception e) {
-//            throw e;
-//        }
-//
-//        String secret = getSecretValueResponse.secretString();
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        DatabaseCredentials credentials;
-//        try {
-//            JsonNode node = mapper.readTree(secret);
-//            credentials = new DatabaseCredentials(
-//                    node.get("DB_URL").asText(),
-//                    node.get("DB_USER").asText(),
-//                    node.get("DB_PASSWORD").asText()
-//            );
-//            System.out.println("node = ");
-//            System.out.println("node = " + node);
-//        } catch (Exception e) {
-//            throw new RuntimeException("secret JSON 구문 분석에 실패했습니다.", e);
-//        }
-//
-//        log.info("데이터베이스 자격 증명을 성공적으로 검색했습니다.");
-//        return credentials;
-//    }
-//}
+package com.megazone.ERPSystem_phase3_Monolithic.common.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
+@Slf4j
+@Configuration
+public class SecretManagerConfig {
+
+    public void getSecret() {
+
+        String secretName = "omz-env-secrets-backend";
+        Region region = Region.of("ap-northeast-2");
+
+        // Create a Secrets Manager client
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(region)
+                .build();
+
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse getSecretValueResponse;
+
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+
+        } catch (Exception e) {
+            // For a list of exceptions thrown, see
+            // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            throw e;
+        }
+
+        String secret = getSecretValueResponse.secretString();
+        log.info(secret);
+
+        // Your code goes here.
+    }
+}
--- src/main/java/com/megazone/ERPSystem_phase3_Monolithic/common/config/aop/LoggingAspect.java
+++ src/main/java/com/megazone/ERPSystem_phase3_Monolithic/common/config/aop/LoggingAspect.java
@@ -1,5 +1,8 @@
 package com.megazone.ERPSystem_phase3_Monolithic.common.config.aop;
 
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.entities.Segment;
+import com.amazonaws.xray.entities.Subsegment;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -20,52 +23,69 @@ public class LoggingAspect {
             "execution(* com.megazone.ERPSystem_phase3_Monolithic.production.controller..*(..))")
     public Object logApiRequest(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodName = joinPoint.getSignature().getName();
-        logger.info("메소드 시작: " + methodName);
+        logger.info("API 메소드 시작: " + methodName);
         Object result;
         try {
             result = joinPoint.proceed(); // 메소드 실행
-            logger.info("메소드 종료: " + methodName);
+            logger.info("API 메소드 종료: " + methodName);
         } catch (Exception e) {
-            logger.error("메소드 실행 중 예외 발생: " + methodName, e);
+            logger.error("API 메소드 실행 중 예외 발생: " + methodName, e);
             throw e;
         }
         return result;
     }
 
-    // 서비스 Layer 메소드 호출 전후로 로깅
+    // 서비스 Layer 메소드 호출 전후로 로깅 및 메인 세그먼트 생성
     @Around("execution(* com.megazone.ERPSystem_phase3_Monolithic.financial.service..*(..)) || " +
             "execution(* com.megazone.ERPSystem_phase3_Monolithic.hr.service..*(..)) || " +
             "execution(* com.megazone.ERPSystem_phase3_Monolithic.logistics.service..*(..)) || " +
             "execution(* com.megazone.ERPSystem_phase3_Monolithic.production.service..*(..))")
     public Object logServiceMethods(ProceedingJoinPoint joinPoint) throws Throwable {
-        String methodName = joinPoint.getSignature().getName();
+        String methodName = joinPoint.getSignature().toShortString();
         logger.info("서비스 메소드 시작: " + methodName);
+
+        // 메인 세그먼트 생성
+        Segment segment = AWSXRay.beginSegment("Service-" + methodName);
         Object result;
         try {
             result = joinPoint.proceed();
             logger.info("서비스 메소드 종료: " + methodName);
         } catch (Exception e) {
+            segment.addException(e); // 예외 기록
             logger.error("서비스 메소드 실행 중 예외 발생: " + methodName, e);
             throw e;
+        } finally {
+            AWSXRay.endSegment(); // 메인 세그먼트 종료
         }
         return result;
     }
 
-    // 리포지토리 Layer 메소드 호출 전후로 로깅
+    // 리포지토리 Layer 메소드 호출 전후로 로깅 및 조건부 서브세그먼트 생성
     @Around("execution(* com.megazone.ERPSystem_phase3_Monolithic.financial.repository..*(..)) || " +
             "execution(* com.megazone.ERPSystem_phase3_Monolithic.hr.repository..*(..)) || " +
             "execution(* com.megazone.ERPSystem_phase3_Monolithic.logistics.repository..*(..)) || " +
             "execution(* com.megazone.ERPSystem_phase3_Monolithic.production.repository..*(..))")
     public Object logRepositoryMethods(ProceedingJoinPoint joinPoint) throws Throwable {
-        String methodName = joinPoint.getSignature().getName();
+        String methodName = joinPoint.getSignature().toShortString();
         logger.info("리포지토리 메소드 시작: " + methodName);
+
+        // 메인 세그먼트가 있는지 확인 후 서브세그먼트 조건부 생성
+        if (!AWSXRay.getCurrentSegmentOptional().isPresent()) {
+//            logger.warn("메인 세그먼트가 없어 서브세그먼트를 생성하지 않고 실행합니다: " + methodName);
+            return joinPoint.proceed();
+        }
+
+        Subsegment subsegment = AWSXRay.beginSubsegment("Repository-" + methodName);
         Object result;
         try {
             result = joinPoint.proceed();
             logger.info("리포지토리 메소드 종료: " + methodName);
         } catch (Exception e) {
+            subsegment.addException(e); // 예외 기록
             logger.error("리포지토리 메소드 실행 중 예외 발생: " + methodName, e);
             throw e;
+        } finally {
+            AWSXRay.endSubsegment(); // 서브세그먼트 종료
         }
         return result;
     }
